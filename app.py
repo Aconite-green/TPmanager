d32e8a329ac9c9ed273c08af8ca6ebdc01fa1a9c
SeongYong Park (uid94157) <seongyong.park@continental-corporation.com>
2024-08-08 12:53:48 +0900

    tutorial 8_8

--- app.py
+++ app.py
@@ -2,27 +2,39 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+# pyqt5-tools designer
+# pyuic5 -x test.ui -o test.py
+
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
-        
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Tech with SeongYong")
+        self.intitUI()
+    
+    def intitUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("my first label")
+        self.label.move(50, 50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("you pressed the button")
+        self.update()
+    
+    def update(self):
+        self.label.adjustSize()
 
 def clicked():
     print("clicked")
 
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("Good Try")
+    win = MyWindow()
 
-    label = QtWidgets.QLabel(win)
-    label.setText("my first label")
-    label.move(50, 50)
-
-    b1 = QtWidgets.QPushButton(win)
-    b1.setText("Click me")
-    b1.clicked.connect(clicked)
     
     win.show()
     sys.exit(app.exec_())
